c0c6e57cc156ef03b4121510ca021b175397ddd3
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-07-02 13:29:10 +1000

    Initial commit
    Demo for repro https://github.com/dotnet/winforms/pull/1254

---
+++ ComboBoxes.Designer.cs
@@ -0,0 +1,90 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace WinformsControlsTest
+{
+    partial class ComboBoxes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "dropdownlist",
+            "aa",
+            "bb",
+            "cc",
+            "d"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 22);
+            this.comboBox1.Name = "comboBox2";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "dropdownlist",
+            "aa",
+            "bb",
+            "cc",
+            "d"});
+            this.comboBox2.Location = new System.Drawing.Point(16, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(250, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // ComboBoxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(831, 198);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Name = "ComboBoxes";
+            this.Text = "ComboBoxes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ ComboBoxes.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace WinformsControlsTest
+{
+    public partial class ComboBoxes : Form
+    {
+        public ComboBoxes()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            var accounts = new List<Account>();
+            var accountNames = new List<string>();
+            Outlook.Application outlook = new Outlook.Application();
+            foreach (Account account in outlook.Session.Accounts)
+            {
+                accounts.Add(account);
+                accountNames.Add(account.DisplayName);
+            }
+
+            comboBox1.DataSource = accountNames;
+
+            comboBox2.DataSource = accounts;
+            comboBox2.DisplayMember = "DisplayName";
+
+            base.OnShown(e);
+        }
+    }
+}
